86f8564a0c3334c7ab497c365409050e7ae2009e
Petter <petter.f.andersson@gmail.com>
2019-08-28 17:09:42 +0200

    Add initial GraphQL schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({
+  schema
+}));
 
 app.listen(3000, () => console.log('Listening for requests on port 3000.'));
