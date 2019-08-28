d406169f20da46b9be5e4dc1b0857f93987ce10a
Petter <petter.f.andersson@gmail.com>
2019-08-28 20:40:46 +0200

    Add ESLint to project

---
+++ server/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018
+  },
+  plugins: ['react'],
+  rules: {
+    'no-useless-catch': 0,
+    'no-unused-vars': 0
+  }
+};
