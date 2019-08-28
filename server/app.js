3db705e8d275e6cd4e8e6fa6ab94faf427171542
Petter <petter.f.andersson@gmail.com>
2019-08-28 16:46:51 +0200

    Add graphql and express-graphql packages

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const app = express();
+
+app.use('/graphql', graphqlHTTP({}));
 
 app.listen(3000, () => console.log('Listening for requests on port 3000.'));
