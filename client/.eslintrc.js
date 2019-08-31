42c8396b987c2ca07420cee6f2ba68e71b530314
Petter <petter.f.andersson@gmail.com>
2019-08-31 17:49:53 +0200

    Create React app, setup Apollo to connect with GraphQL

---
+++ client/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'prettier/react',
+    'plugin:react/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018
+  },
+  plugins: ['react/recommended'],
+  rules: {
+    'no-useless-catch': 0,
+    'no-unused-vars': 0
+  }
+};
--- server/app.js
+++ server/app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`,
@@ -13,4 +16,4 @@ mongoose.connect(
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
-app.listen(3000, () => console.log('Listening for requests on port 3000.'));
+app.listen(4000, () => console.log('Listening for requests on port 4000.'));
